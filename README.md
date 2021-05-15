190a45acc93efe54403db5e306c67f1dff656122
Cebrail Erdogan <cebonet@msn.com>
2021-05-15 14:26:50 +0200

    refactor project to capacitor

---
+++ README.md
@@ -0,0 +1,22 @@
+# project setup
+This one runs Capacitor and Angular
+
+# Fetch dependencies
+npm install
+
+# Build
+ionic build
+
+# Build for android
+npx cap sync android
+or
+ionic capacitor build android
+
+## open android studio
+npx cap open android
+
+and wait for gradle to build the project
+
+
+
+
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { AudioRecorder} from '../home/audioRecorder';
+import { Note } from "../home/note";
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+  ],
+  providers: [
+    AudioRecorder,
+    Note,
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
