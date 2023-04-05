399f8addfc1f897d2586fd7d3b9c885ece209d49
Cebrail Erdogan <erdogan.cebrail@gmail.com>
2023-04-05 02:12:38 +0200

    Switched to blak and white theme similar to icon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AudioRecorder } from './audioRecorder';
 import { Note } from "./note";
 import { instruments } from "src/assets/config/arrangements.json";
+import { body } from 'node_modules.backup/ionicons/icons';
+import { main } from 'node_modules.backup/figures';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,7 @@ import { instruments } from "src/assets/config/arrangements.json";
 })
 export class HomePage {
   circleCenterPos: number;
-  frequencyCache: Array<number> = new Array(50);;
+  frequencyCache: Array<number> = new Array(50);
   amplitudeLimit: number = 0.01;
   tuned: boolean = true;
   perfectlyTuned: boolean = true;
@@ -22,7 +24,7 @@ export class HomePage {
   constructor(private audioRecorder: AudioRecorder, private noteTools: Note,) {
 
     // Setup frequency cache with limit 5
-    this.frequencyCache.push = function () {
+    this.frequencyCache.push = function() {
       if (this.length >= 50) {
         this.shift();
       }
@@ -50,7 +52,7 @@ export class HomePage {
     }
 
 
-    if (diffFromFundemental < 0.08) {
+    if (diffFromFundemental < 0.1) {
       this.perfectlyTuned = true;
       this.tuned = true;
     }
@@ -62,21 +64,27 @@ export class HomePage {
       this.perfectlyTuned = false;
     }
 
-    var note = this.noteTools.getMIDIasNote(frequency, true);
+    var note = this.noteTools.getMIDIasNote(frequency);
     var frequency = frequency.toFixed(1);
     var octave = this.noteTools.getOctave(frequency);
     var tuneComment, tuneComment2;
 
     if (document.getElementById("note") != null) {
-      document.getElementById("note").textContent = note;
+      document.getElementById("note").textContent = note + octave;
     }
 
-    if (document.getElementById("octave") != null) {
-      document.getElementById("octave").textContent = "Octave: " + octave.toString();
+
+    if (document.getElementById("diff") != null) {
+      document.getElementById("diff").textContent = "Diff: " + this.noteTools.getDistanceToCurrentPitch(frequency).toFixed(1).toString();
     }
 
     if (document.getElementById("frequency") != null) {
       document.getElementById("frequency").textContent = frequency + "Hz";
+    }
+
+    if (document.getElementById("frequencyRange") != null) {
+      var min = this.noteTools.getPreviousFundementalFrequency(frequency);
+      document.getElementById("frequencyRange").textContent = min + "Hz";
     }
 
     if (document.getElementById("circle") != null) {
@@ -84,11 +92,12 @@ export class HomePage {
       var currentFundementalFrequency = this.noteTools.getFundamentalFrequency(frequency);
       var previousFundementalFrequency = this.noteTools.getPreviousFundementalFrequency(frequency);
       var nextFundementalFrequency = this.noteTools.getNextFundementalFrequency(frequency);
-      this.moveCircle(frequency, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle);
+      //this.moveCircle(frequency, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle);
+      this.sizeCircle(frequency, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle);
       this.colorCircle(circle);
       if (document.getElementById("tuneComment") != null) {
 
-        if(!this.perfectlyTuned){
+        if (!this.perfectlyTuned) {
           if (frequency > currentFundementalFrequency) {
             tuneComment2 = "tune down a bit"
           } else {
@@ -112,7 +121,7 @@ export class HomePage {
 
   }
 
-  private averageOffset = function () {
+  private averageOffset = function() {
     let offsetCache = Object.assign([], this.frequencyCache);
     offsetCache = offsetCache.map(f => f - this.noteTools.getFundamentalFrequency(f).toFixed(2));
     let avg = (offsetCache.reduce((a, b) => a + b, 0) / offsetCache.length);
@@ -120,33 +129,55 @@ export class HomePage {
   }
 
 
-  public colorCircle = function (circle: HTMLElement) {
+  public colorCircle = function(circle: HTMLElement) {
     if (this.perfectlyTuned) {
       circle.style.backgroundColor = 'rgba(62, 153, 48, 0.8)';
       // circle.style.boxShadow = '0em 0em 20px 20px rgba(0, 0, 0, 0.325)';
-    }
-    else if (this.tuned) {
-      circle.style.backgroundColor = 'rgba(197, 61, 61, 0.8)'; 
-      // circle.style.boxShadow = '0em 0em 5px 5px rgba(0, 0, 0, 0.325)';
     } else {
       circle.style.backgroundColor = 'rgb(192, 57, 43)';
       // circle.style.boxShadow = '0em 0em 10px 8px rgba(255, 0, 0, 0.125)';
     }
   }
 
-  public moveCircle = function (frequency: number, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle: HTMLElement) {
+  public moveCircle = function(frequency: number, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle: HTMLElement) {
     let center = 50
-    let position = -1 * (this.noteTools.getFundamentalFrequency(frequency) - frequency);
+    let amplify_finetunning = 1.5;
+    let amplify = 1;
+
+    if (Math.abs(this.noteTools.getFundamentalFrequency(frequency) - frequency) < 1) {
+      //amplify = amplify_finetunning;
+    }
+
+    let position = (frequency - this.noteTools.getFundamentalFrequency(frequency)) * amplify;
     let newPosition = (center + position);
 
-    if (newPosition < 20 || newPosition > 80) {
+    if (newPosition < 10 || newPosition > 90) {
       return;
     }
 
+
+
     circle.style.left = 'calc(' + newPosition.toFixed(3) + '%)';
 
-    console.log('current fun=' + currentFundementalFrequency, '  prev fun= ' + previousFundementalFrequency + '  next fun= ' + nextFundementalFrequency)
+    //console.log('current fun=' + currentFundementalFrequency, '  prev fun= ' + previousFundementalFrequency + '  next fun= ' + nextFundementalFrequency)
     console.log('new position:' + newPosition);
+  }
+
+  public sizeCircle = function(frequency: number, currentFundementalFrequency, previousFundementalFrequency, nextFundementalFrequency, circle: HTMLElement) {
+    let minSize = 60;
+    let maxSize = 130;
+    let diff = minSize + this.noteTools.getFundamentalFrequency(frequency) - frequency;
+
+    if (diff > maxSize) {
+      diff = maxSize;
+    }
+
+    if (diff > minSize && diff < maxSize) {
+      circle.style.width = 'calc(' + diff.toFixed(3) + 'vh)';
+      circle.style.height = 'calc(' + diff.toFixed(3) + 'vh)';
+    }
+
+    console.log('diff:' + diff);
   }
 
 }
--- src/app/home/note.ts
+++ src/app/home/note.ts
@@ -1,72 +1,78 @@
 // https://en.wikipedia.org/wiki/MIDI_tuning_standard
+import * as midi_notes from 'src/assets/midi_table.json';
+
 const base_a4 = 440;
+const min_midi_num = 8;
+const max_midi_num = 127;
+const adjacent_freq_ratio = Math.pow(2, 16 / 12);
 
 export class Note {
-    constructor(){};
+  constructor() { };
 
-    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-    notesSolfeg = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"];
+  notes_western = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  notes_eastern = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"];
 
-    // 440 -> 69
-    getMIDI = function (f) {
-        return Math.round(69 + 12 * Math.log2(f / base_a4));
-    }
+  // 440Hz (does not need to be pitch) -> 69
+  getMIDI = function(f: number) {
+    return Math.round(69 + 12 * Math.log2(f / base_a4));
+  }
 
-    // 440Hz -> "Mi"
-    getMIDIasNote = function(f,isSolfeg){
-        let n = this.getMIDI(f)
-        if (n >= 0 && n <= 119){
-            if(isSolfeg){
-                return this.notesSolfeg[n % 12]
-            }
-            else{
-                return this.notes[n % 12]
-            };
-        }
-        return "N/A";
+  // "69" -> 440Hz
+  getMIDIToFrequency = function(n: number) {
+    var result = 0;
+    if (n >= min_midi_num && n <= max_midi_num) {
+      result = base_a4 * Math.pow(2, (n - 69) / 12);
     }
-   
-    // 440 -> 4
-    getOctave = function(f){
-        let n = this.getMIDI(f);
-        return  Math.floor((n/12)-1);
-    }
-   
-    // "69" -> 440
-    getMIDIToFrequency = function (n){
-        var result= 0;
-        if (n >= 0 && n <= 128){
-            result = base_a4 * Math.pow(2, (n-69)/12 );
-        }
-        return result;
-    }
+    return result;
+  }
 
-    // 445 -> 440
-    getFundamentalFrequency = function(f){
-        return this.getMIDIToFrequency(this.getMIDI(f));
+  // 440Hz -> "Mi"
+  getMIDIasNote = function(f: number) {
+    let n = this.getMIDI(f)
+    if (n >= 0 && n <= 119) {
+      return this.notes_eastern[n % 12]
     }
+    return "N/A";
+  }
 
-    getNoteScaleSizeFromCenter = function(f){
-       //Distance to next neighbour
-       return this.getMIDIToFrequency(this.getMIDI(f) + 1) - this.getFundamentalFrequency(f);
-    }
+  // 440Hz -> 4
+  getOctave = function(f: number) {
+    let n = this.getMIDI(f);
+    return Math.floor((n / 12) - 1);
+  }
 
-    getNextFundementalFrequency = function(f){
-       return this.getMIDIToFrequency(this.getMIDI(f) + 1);
-    }
-    
-    getPreviousFundementalFrequency = function(f){
-       return this.getMIDIToFrequency(this.getMIDI(f) - 1);
-    }
+  // 445Hz -> 440Hz
+  getFundamentalFrequency = function(f: number) {
+    return this.getMIDIToFrequency(this.getMIDI(f));
+  }
 
-    getOutOfTuneDifference = function(f){
-        let MIDI = this.getMIDI(f);
-        let fundementalBefore = this.getMIDIToFrequency(MIDI-1);
-        let fundementalAfter = this.getMIDIToFrequency(MIDI+1);
+  getNextFundementalFrequency = function(f: number) {
+    return this.getMIDIToFrequency(this.getMIDI(f) + 1);
+  }
 
-        return fundementalBefore  + " " + 
-               f + " " + 
-               fundementalAfter + " /n";
-    }
+  getPreviousFundementalFrequency = function(f: number) {
+    return this.getMIDIToFrequency(this.getMIDI(f) - 1);
+  }
 
+  getDistanceToCurrentPitch = function(f: number) {
+    return f - this.getFundamentalFrequency(f);
+  }
+
+  getDistanceToNextPitch = function(f: number) {
+    return this.getNextFundementalFrequency(f) - this.getFundamentalFrequency(f);
+  }
+
+  getDistanceToPreviousPitch = function(f: number) {
+    return this.getFundamentalFrequency(f) - this.getPreviousFundementalFrequency(f);
+  }
+
+  getOutOfTuneDifference = function(f: number) {
+    let MIDI = this.getMIDI(f);
+    let fundementalBefore = this.getMIDIToFrequency(MIDI - 1);
+    let fundementalAfter = this.getMIDIToFrequency(MIDI + 1);
+
+    return fundementalBefore + " " +
+      f + " " +
+      fundementalAfter + " /n";
+  }
 }
